577ffab22385ddf50e2805129bbaac3bd492f108
Victoria Amirov <vika.amir20@gmail.com>
2023-06-15 09:24:19 -0700

    user input

--- play.js
+++ play.js
@@ -14,17 +14,39 @@ const connect = function () {
     console.log("connecting to the server")
   });
   conn.on ("data", (data) => {
-    console.log("Message from client: ", data);
+    console.log("Message from client: ", data)
   });
   conn.on ("connect", () => {
     console.log("Successfully connected to game server");
+    conn.write("Name: VIK");
   });
-    conn.on ("connect", () => {
-      conn.write("Name: VIK");
-    });
-  
+
   return conn;
   
-}
+  }
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+  };
+  
+  const handleUserInput = function () {
+    stdin.on("data", (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+  
+  // Call the setupInput function
+  const stdin = setupInput();
+  // Call the handleUserInput function
+  handleUserInput();
+    
+
+  
+
 console.log("Connecting ...");
 connect();
